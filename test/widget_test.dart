031dc21913a663d8b08cc1e80bba3943b0d99374
sacheen <baralisachin@gmail.com>
2023-06-27 20:44:24 +0545

    test changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:add_your_expenses/widgets/expenses_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:add_your_expenses/main.dart';
+// import 'package:add_your_expenses/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
